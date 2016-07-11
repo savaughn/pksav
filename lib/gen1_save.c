816b4c2b388357644de2a5dfc7c66f367aeafc98
Nicholas Corgan <n.corgan@gmail.com>
2016-07-11 09:39:53 -0500

    gen1_save: set pointers in load function

--- lib/gen1_save.c
+++ lib/gen1_save.c
@@ -43,6 +43,7 @@ pksav_error_t pksav_gen1_save_load(
     const char* filepath,
     pksav_gen1_save_t* gen1_save
 ) {
+    // Read the file and make sure it's valid
     FILE* gen1_save_file = fopen(filepath, "r");
     fseek(gen1_save_file, SEEK_END, 0);
 
@@ -59,6 +60,35 @@ pksav_error_t pksav_gen1_save_load(
         free(gen1_save->raw);
         return PKSAV_ERROR_INVALID_SAVE;
     }
+
+    /*
+     * Check if this save is for the Yellow version. The only way to check this is to check the
+     * Pikachu Friendship field, which isn't used in Red/Blue. This is usually fine but will fail
+     * if the trainer's Pikachu despises the trainer enough to have a friendship value of 0, which
+     * is unlikely but technically possible.
+     */
+    gen1_save->yellow = (gen1_save->raw[PKSAV_GEN1_PIKACHU_FRIENDSHIP] > 0);
+
+    // Set pointers
+    gen1_save->pokemon_party = (pksav_gen1_pokemon_party_t*)&gen1_save->raw[PKSAV_GEN1_POKEMON_PARTY];
+
+    gen1_save->pokemon_boxes = malloc(sizeof(pksav_gen1_pokemon_box_t*)*12);
+    for(uint8_t i = 0; i < 6; ++i) {
+        uint16_t offset = PKSAV_GEN1_POKEMON_PC_FIRST_HALF + (sizeof(pksav_gen1_pokemon_box_t)*i);
+        gen1_save->pokemon_boxes[i] = (pksav_gen1_pokemon_box_t*)&gen1_save->raw[offset];
+    }
+    for(uint8_t i = 6; i < 12; ++i) {
+        uint16_t offset = PKSAV_GEN1_POKEMON_PC_SECOND_HALF + (sizeof(pksav_gen1_pokemon_box_t)*(i-6));
+        gen1_save->pokemon_boxes[i] = (pksav_gen1_pokemon_box_t*)&gen1_save->raw[offset];
+    }
+
+    gen1_save->item_bag = (pksav_gen1_item_bag_t*)&gen1_save->raw[PKSAV_GEN1_ITEM_BAG];
+    gen1_save->item_pc = (pksav_gen1_item_pc_t*)&gen1_save->raw[PKSAV_GEN1_ITEM_PC];
+    gen1_save->time_played = (pksav_gen1_time_t*)&gen1_save->raw[PKSAV_GEN1_TIME_PLAYED];
+    gen1_save->money = &gen1_save->raw[PKSAV_GEN1_MONEY];
+    gen1_save->trainer_id = (uint16_t*)&gen1_save->raw[PKSAV_GEN1_PLAYER_ID];
+    gen1_save->trainer_name = &gen1_save->raw[PKSAV_GEN1_PLAYER_NAME];
+    gen1_save->rival_name = &gen1_save->raw[PKSAV_GEN1_RIVAL_NAME];
 
     return PKSAV_ERROR_NONE;
 }
