e3eedb64e76ae3a8fbb4a146715476267eac1476
Nicholas Corgan <n.corgan@gmail.com>
2016-07-24 14:52:03 -0500

    Removed superfluous error codes

--- lib/stats.c
+++ lib/stats.c
@@ -63,7 +63,7 @@ pksav_error_t pksav_set_gb_IV(
     uint8_t new_IV
 ) {
     if(new_IV > 15) {
-        return PKSAV_ERROR_INVALID_IV;
+        return PKSAV_ERROR_PARAM_OUT_OF_RANGE;
     }
 
     switch(stat) {
@@ -149,7 +149,7 @@ pksav_error_t pksav_set_IV(
     uint8_t new_IV
 ) {
     if(new_IV > 31) {
-        return PKSAV_ERROR_INVALID_IV;
+        return PKSAV_ERROR_PARAM_OUT_OF_RANGE;
     }
 
     switch(stat) {
