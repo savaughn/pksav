2353b3e3d658e0a52fc4e356325734f59132b325
Nicholas Corgan <n.corgan@gmail.com>
2016-10-03 06:26:24 -0500

    apps: added empty Gen IV dump app

---
+++ apps/pksav-gen4-save-dump.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+#include <pksav/gen4.h>
+
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    return EXIT_SUCCESS;
+}
---
+++ include/pksav/common/coordinates.h
@@ -0,0 +1,14 @@
+/*!
+ * @file    pksav/common/coordinates.h
+ * @ingroup PKSav
+ * @brief   Native storage for (x,y,z) coordinates.
+ *
+ * Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+#ifndef PKSAV_COMMON_COORDINATES_H
+#define PKSAV_COMMON_COORDINATES_H
+
+#endif /* PKSAV_COMMON_COORDINATES_H */
