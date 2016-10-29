4bd226096cf80664ff5bb3dbdaada6a70a7b3d0b
Nicholas Corgan <n.corgan@gmail.com>
2016-10-29 12:47:14 -0500

    appveyor: still working on ARM check

--- testing/appveyor-scripts/build-test.bat
+++ testing/appveyor-scripts/build-test.bat
@@ -5,7 +5,7 @@ cd c:\projects\pksav\build
 cmake -G "%CMAKE_DASH_G%" ..
 msbuild ALL_BUILD.vcxproj
 
-if x%CMAKE_DASH_G:ARM=%==x%CMAKE_DASH_G% (
+if "x%CMAKE_DASH_G:ARM=%"=="x%CMAKE_DASH_G%" (
     ctest --output-on-failure
 )
 
