f2dc2f6fe2d734ab81a298a592281ebb7dfa0768
Nicholas Corgan <n.corgan@gmail.com>
2016-10-29 12:30:13 -0500

    appveyor: remove invalid cases

--- testing/appveyor-scripts/build-test.bat
+++ testing/appveyor-scripts/build-test.bat
@@ -2,4 +2,7 @@ mkdir c:\projects\pksav\build
 cd c:\projects\pksav\build
 cmake -G "%CMAKE_DASH_G%" ..
 msbuild ALL_BUILD.vcxproj
-ctest --output-on-failure
+
+if x%CMAKE_DASH_G:ARM=%==x%CMAKE_DASH_G% (
+    ctest --output-on-failure
+)
