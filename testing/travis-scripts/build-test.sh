c993b8a956b0b75f9980e733d4aa1397147f0314
Nicholas Corgan <n.corgan@gmail.com>
2016-11-11 05:46:03 -0600

    travis: Valgrind all apps

--- testing/travis-scripts/build-test.sh
+++ testing/travis-scripts/build-test.sh
@@ -19,34 +19,55 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     ctest --output-on-failure
 
 else
-    find $REPO_TOPLEVEL -name '*.[ch]' | xargs cppcheck 1>/dev/null
+    # Check source
+    find $REPO_TOPLEVEL -name '*.[ch]' | xargs cppcheck --error-exitcode=1 --force 1>/dev/null
     [ $? -ne 0 ] && exit 1
 
     mkdir gcc clang mingw
 
+    # GCC compile check
     cd $REPO_TOPLEVEL/travis-env/gcc
     [ $? -ne 0 ] && exit 1
-    cmake $REPO_TOPLEVEL
+    cmake -DCMAKE_BUILD_TYPE=Debug $REPO_TOPLEVEL
     [ $? -ne 0 ] && exit 1
     make
     [ $? -ne 0 ] && exit 1
     ctest --output-on-failure
     [ $? -ne 0 ] && exit 1
 
+    # Clang compile check
     cd $REPO_TOPLEVEL/travis-env/clang
     [ $? -ne 0 ] && exit 1
-    CC=clang cmake $REPO_TOPLEVEL
+    CC=clang cmake -DCMAKE_BUILD_TYPE=Debug $REPO_TOPLEVEL
     [ $? -ne 0 ] && exit 1
     make
     [ $? -ne 0 ] && exit 1
     ctest --output-on-failure
     [ $? -ne 0 ] && exit 1
 
+    # MinGW-GCC compile check
     cd $REPO_TOPLEVEL/travis-env/mingw
     [ $? -ne 0 ] && exit 1
     cmake -DCMAKE_TOOLCHAIN_FILE=$REPO_TOPLEVEL/cmake/Toolchains/mingw_cross.cmake \
-	  -DMINGW_PREFIX=x86_64-w64-mingw32 \
-	  $REPO_TOPLEVEL
+          -DMINGW_PREFIX=x86_64-w64-mingw32 \
+          -DCMAKE_BUILD_TYPE=Debug \
+          $REPO_TOPLEVEL
     [ $? -ne 0 ] && exit 1
     make
+    [ $? -ne 0 ] && exit 1
+
+    # Set up runtime testing
+    cd $REPO_TOPLEVEL/travis-env
+    export SAVEDIR=$REPO_TOPLEVEL/testing/pksav-test-saves
+    export PATH=$PWD/gcc/apps:$PATH
+    export LD_LIBRARY_PATH=$PWD/gcc/lib:$LD_LIBRARY_PATH
+
+    # App testing
+    valgrind --leak-check=full --track-origins=yes --error-exitcode=1 pksav-gen1-save-dump --all --input=$SAVEDIR/red_blue/pokemon_red.sav
+    [ $? -ne 0 ] && exit 1
+    valgrind --leak-check=full --track-origins=yes --error-exitcode=1 pksav-gen1-save-dump --all --input=$SAVEDIR/yellow/pokemon_yellow.sav
+    [ $? -ne 0 ] && exit 1
+    valgrind --leak-check=full --track-origins=yes --error-exitcode=1 pksav-gen2-save-dump --all --input=$SAVEDIR/gold_silver/pokemon_gold.sav
+    [ $? -ne 0 ] && exit 1
+    valgrind --leak-check=full --track-origins=yes --error-exitcode=1 pksav-gen2-save-dump --all --input=$SAVEDIR/crystal/pokemon_crystal.sav
 fi
