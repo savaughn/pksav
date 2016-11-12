5f6cc0de4f4cad69d3b3363b56bee453ff3cf111
Nicholas Corgan <n.corgan@gmail.com>
2016-11-12 10:13:12 -0600

    appveyor: more attempts to call apps

--- testing/appveyor-scripts/build-test.bat
+++ testing/appveyor-scripts/build-test.bat
@@ -14,23 +14,22 @@ if not %ERRORLEVEL%==0 goto fail
 
 :: Don't run anything when cross-compiling
 if "x%CMAKE_DASH_G:ARM=%"=="x%CMAKE_DASH_G%" (
-    start /WAIT ctest --output-on-failure
+    ctest --output-on-failure
     if not %ERRORLEVEL%==0 goto fail
 
     set SAVEDIR=c:\projects\pksav\testing\pksav-test-saves
-    set PATH=c:\projects\pksav\test-env\build\lib\Debug;"%PATH%"
-    set PATH=c:\projects\pksav\test-env\build\apps\Debug;"%PATH%"
+    set PATH="%PATH%";c:\projects\pksav\test-env\build\lib\Debug;c:\projects\pksav\test-env\build\apps\Debug
 
-    start /WAIT pksav-gen1-save-dump --all --input=%SAVEDIR%\red_blue\pokemon_red.sav
+    pksav-gen1-save-dump --all --input="%SAVEDIR%\red_blue\pokemon_red.sav"
     if not %ERRORLEVEL%==0 goto fail
 
-    start /WAIT pksav-gen1-save-dump --all --input=%SAVEDIR%\yellow\pokemon_yellow.sav
+    pksav-gen1-save-dump --all --input="%SAVEDIR%\yellow\pokemon_yellow.sav"
     if not %ERRORLEVEL%==0 goto fail
 
-    start /WAIT pksav-gen2-save-dump %SAVEDIR%\gold_silver\pokemon_gold.sav
+    pksav-gen2-save-dump "%SAVEDIR%\gold_silver\pokemon_gold.sav"
     if not %ERRORLEVEL%==0 goto fail
 
-    start /WAIT pksav-gen2-save-dump %SAVEDIR%\crystal\pokemon_crystal.sav
+    pksav-gen2-save-dump "%SAVEDIR%\crystal\pokemon_crystal.sav"
     if not %ERRORLEVEL%==0 goto fail
 )
 
