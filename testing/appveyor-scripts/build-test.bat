f80d54d52877f0a7d72484cf0aa76fefe4cb753f
Nicholas Corgan <n.corgan@gmail.com>
2016-11-12 06:47:52 -0600

    appveyor: fix msbuild call

--- testing/appveyor-scripts/build-test.bat
+++ testing/appveyor-scripts/build-test.bat
@@ -8,7 +8,7 @@ if not %ERRORLEVEL%==0 goto fail
 cmake -G "%CMAKE_DASH_G%" -DCMAKE_BUILD_TYPE=Debug ..\..
 if not %ERRORLEVEL%==0 goto fail
 
-msbuild ALL_BUILD.vcxproj /p:configuration=Debug
+msbuild /v:n /p:configuration=Debug ALL_BUILD.vcxproj
 if not %ERRORLEVEL%==0 goto fail
 
 :: Don't run anything when cross-compiling
