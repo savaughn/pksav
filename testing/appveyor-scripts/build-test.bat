1fb330fb47c5ea864458190e610eecf0d8297f2c
Nicholas Corgan <n.corgan@gmail.com>
2016-11-12 06:30:07 -0600

    appveyor: try to detect failure

--- testing/appveyor-scripts/build-test.bat
+++ testing/appveyor-scripts/build-test.bat
@@ -5,8 +5,19 @@ cd c:\projects\pksav\test-env\build
 cmake -G "%CMAKE_DASH_G%" ..\..
 msbuild ALL_BUILD.vcxproj
 
+if not %ERRORLEVEL%==0 goto fail
+
 if "x%CMAKE_DASH_G:ARM=%"=="x%CMAKE_DASH_G%" (
     ctest --output-on-failure
+    if not %ERRORLEVEL%==0 goto fail
 )
 
+goto pass
+
 endlocal
+
+:fail
+exit /b 1
+
+:pass
+exit /b 0
