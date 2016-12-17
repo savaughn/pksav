7d8b70e260a2f4b5c4018b0e5c4a90455dc78b94
Nicholas Corgan <n.corgan@gmail.com>
2016-12-17 09:55:36 -0600

    appveyor: attempting to test with MinGW

--- testing/appveyor-scripts/build-test.bat
+++ testing/appveyor-scripts/build-test.bat
@@ -6,14 +6,19 @@ mkdir c:\projects\pksav\test-env\build
 cd c:\projects\pksav\test-env\build
 if not !ERRORLEVEL!==0 goto fail
 
-cmake -G "%CMAKE_DASH_G%" -DCMAKE_BUILD_TYPE=Release ..\..
+cmake -G "%GENERATOR_NAME%" -DCMAKE_BUILD_TYPE=Release ..\..
 if not !ERRORLEVEL!==0 goto fail
 
-msbuild /p:configuration=Release ALL_BUILD.vcxproj
+:: Generator-specific build command
+if %GENERATOR_NAME%=="MinGW Makefiles" (
+    make
+) else (
+    msbuild /p:configuration=Release ALL_BUILD.vcxproj
+)
 if not !ERRORLEVEL!==0 goto fail
 
 :: Don't run anything when cross-compiling
-if "x%CMAKE_DASH_G:ARM=%"=="x%CMAKE_DASH_G%" (
+if "x%GENERATOR_NAME:ARM=%"=="x%GENERATOR_NAME%" (
     ctest --output-on-failure
     if not !ERRORLEVEL!==0 goto fail
 )
