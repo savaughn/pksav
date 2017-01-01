d599a70f5c31126cb2de17028a047099d9d1270b
Nicholas Corgan <n.corgan@gmail.com>
2017-01-01 13:19:10 -0600

    Starting save_detection_test

---
+++ testing/unit-tests/save_detection_test.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+#include "c_test_common.h"
+
+#include <pksav.h>
+
+static void pksav_gen1_save_detection_test() {
+}
+
+PKSAV_TEST_MAIN(
+    PKSAV_TEST(pksav_gen1_save_detection_test)
+)
