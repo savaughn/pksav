028a9b0cd13a32021eef92eb72f3addb21bf7324
Nicholas Corgan <n.corgan@gmail.com>
2017-03-01 21:18:46 -0600

    travis: as with LibPKMN, ignore OS X

--- testing/travis-scripts/build-test.sh
+++ testing/travis-scripts/build-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
+# Copyright (c) 2016-2017 Nicholas Corgan (n.corgan@gmail.com)
 #
 # Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
 # or copy at http://opensource.org/licenses/MIT)
@@ -11,44 +11,32 @@ mkdir -p test-env
 cd test-env
 [ $? -ne 0 ] && exit 1
 
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    # Compile test
-    mkdir -p build
-    cd build
+# Check source
+find $REPO_TOPLEVEL -name '*.[ch]' | xargs cppcheck --error-exitcode=1 --force 1>/dev/null
+[ $? -ne 0 ] && exit 1
 
-    cmake $REPO_TOPLEVEL
-    [ $? -ne 0 ] && exit 1
-    make
-    [ $? -ne 0 ] && exit 1
-    ctest --output-on-failure
-    [ $? -ne 0 ] && exit 1
-else
-    # Check source
-    find $REPO_TOPLEVEL -name '*.[ch]' | xargs cppcheck --error-exitcode=1 --force 1>/dev/null
-    [ $? -ne 0 ] && exit 1
-
-    if [ "$NEWCC" != "" ]
-    then
-        export CC=$NEWCC
-    fi
-
-    cmake $REPO_TOPLEVEL
-    [ $? -ne 0 ] && exit 1
-    make
-    [ $? -ne 0 ] && exit 1
-    ctest --output-on-failure
-    [ $? -ne 0 ] && exit 1
-
-    # MinGW-GCC compile check
-    #cd $REPO_TOPLEVEL/test-env/mingw
-    #[ $? -ne 0 ] && exit 1
-    #cmake -DCMAKE_TOOLCHAIN_FILE=$REPO_TOPLEVEL/cmake/Toolchains/mingw_cross.cmake \
-    #      -DMINGW_PREFIX=x86_64-w64-mingw32 \
-    #      -DCMAKE_BUILD_TYPE=Debug \
-    #      $REPO_TOPLEVEL
-    #[ $? -ne 0 ] && exit 1
-    #make
-    #[ $? -ne 0 ] && exit 1
+if [ "$NEWCC" != "" ]
+then
+export CC=$NEWCC
 fi
+
+# Build and test
+cmake $REPO_TOPLEVEL
+[ $? -ne 0 ] && exit 1
+make
+[ $? -ne 0 ] && exit 1
+ctest --output-on-failure
+[ $? -ne 0 ] && exit 1
+
+# MinGW-GCC compile check
+#cd $REPO_TOPLEVEL/test-env/mingw
+#[ $? -ne 0 ] && exit 1
+#cmake -DCMAKE_TOOLCHAIN_FILE=$REPO_TOPLEVEL/cmake/Toolchains/mingw_cross.cmake \
+#      -DMINGW_PREFIX=x86_64-w64-mingw32 \
+#      -DCMAKE_BUILD_TYPE=Debug \
+#      $REPO_TOPLEVEL
+#[ $? -ne 0 ] && exit 1
+#make
+#[ $? -ne 0 ] && exit 1
 
 echo # So we can check the last error code
--- testing/travis-scripts/install-deps.sh
+++ testing/travis-scripts/install-deps.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 #
-# Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
+# Copyright (c) 2016-2017 Nicholas Corgan (n.corgan@gmail.com)
 #
 # Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
 # or copy at http://opensource.org/licenses/MIT)
 #
 
-REPO_TOPLEVEL=$PWD
-
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-
-    brew update
-    brew install doxygen
-
-else
-    sudo apt-get -y update
-    sudo apt-get -y install build-essential cmake clang gcc \
-                            gcc-mingw-w64-x86-64 cppcheck \
-                            valgrind doxygen
-fi
+sudo apt-get -y update
+sudo apt-get -y install build-essential cmake clang gcc \
+		    gcc-mingw-w64-x86-64 cppcheck \
+		    valgrind doxygen
