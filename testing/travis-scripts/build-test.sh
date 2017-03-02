118ad522c59a798adbd3b0c8deed66e417bfbe31
Nicholas Corgan <n.corgan@gmail.com>
2017-03-01 20:51:31 -0600

    travis: starting to bring in build configurations from LibPKMN

--- testing/travis-scripts/build-test.sh
+++ testing/travis-scripts/build-test.sh
@@ -16,7 +16,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     mkdir -p build
     cd build
 
-    cmake -DCMAKE_BUILD_TYPE=Debug $REPO_TOPLEVEL
+    cmake $REPO_TOPLEVEL
     [ $? -ne 0 ] && exit 1
     make
     [ $? -ne 0 ] && exit 1
@@ -27,22 +27,12 @@ else
     find $REPO_TOPLEVEL -name '*.[ch]' | xargs cppcheck --error-exitcode=1 --force 1>/dev/null
     [ $? -ne 0 ] && exit 1
 
-    mkdir -p gcc clang mingw
+    if [ "$NEWCC" != "" ]
+    then
+        export CC=$NEWCC
+    fi
 
-    # GCC compile check
-    cd $REPO_TOPLEVEL/test-env/gcc
-    [ $? -ne 0 ] && exit 1
-    cmake -DCMAKE_BUILD_TYPE=Debug $REPO_TOPLEVEL
-    [ $? -ne 0 ] && exit 1
-    make
-    [ $? -ne 0 ] && exit 1
-    ctest --output-on-failure
-    [ $? -ne 0 ] && exit 1
-
-    # Clang compile check
-    cd $REPO_TOPLEVEL/test-env/clang
-    [ $? -ne 0 ] && exit 1
-    CC=clang cmake -DCMAKE_BUILD_TYPE=Debug $REPO_TOPLEVEL
+    cmake $REPO_TOPLEVEL
     [ $? -ne 0 ] && exit 1
     make
     [ $? -ne 0 ] && exit 1
@@ -50,15 +40,15 @@ else
     [ $? -ne 0 ] && exit 1
 
     # MinGW-GCC compile check
-    cd $REPO_TOPLEVEL/test-env/mingw
-    [ $? -ne 0 ] && exit 1
-    cmake -DCMAKE_TOOLCHAIN_FILE=$REPO_TOPLEVEL/cmake/Toolchains/mingw_cross.cmake \
-          -DMINGW_PREFIX=x86_64-w64-mingw32 \
-          -DCMAKE_BUILD_TYPE=Debug \
-          $REPO_TOPLEVEL
-    [ $? -ne 0 ] && exit 1
-    make
-    [ $? -ne 0 ] && exit 1
+    #cd $REPO_TOPLEVEL/test-env/mingw
+    #[ $? -ne 0 ] && exit 1
+    #cmake -DCMAKE_TOOLCHAIN_FILE=$REPO_TOPLEVEL/cmake/Toolchains/mingw_cross.cmake \
+    #      -DMINGW_PREFIX=x86_64-w64-mingw32 \
+    #      -DCMAKE_BUILD_TYPE=Debug \
+    #      $REPO_TOPLEVEL
+    #[ $? -ne 0 ] && exit 1
+    #make
+    #[ $? -ne 0 ] && exit 1
 fi
 
 echo # So we can check the last error code
