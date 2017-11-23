ce8a614524a87715338fad36aed737e43c60d94b
Nicholas Corgan <n.corgan@gmail.com>
2017-11-23 11:28:53 -0800

    gen6: added ribbon enums

--- include/pksav/gen6.h
+++ include/pksav/gen6.h
@@ -17,6 +17,7 @@
 #include <pksav/gen6/hall_of_fame.h>
 #include <pksav/gen6/items.h>
 #include <pksav/gen6/pokemon.h>
+#include <pksav/gen6/ribbons.h>
 #include <pksav/gen6/text.h>
 
 #include <pksav/common/condition.h>
