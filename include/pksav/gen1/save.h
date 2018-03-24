a6ca50eb087593870142da62d8d83efdc758f356
Nicholas Corgan <n.corgan@gmail.com>
2018-03-24 06:13:57 -0500

    gen1: separated badge enum into own header

--- include/pksav/gen1/save.h
+++ include/pksav/gen1/save.h
@@ -10,6 +10,7 @@
 #include <pksav/config.h>
 #include <pksav/error.h>
 
+#include <pksav/gen1/badges.h>
 #include <pksav/gen1/items.h>
 #include <pksav/gen1/pokemon.h>
 #include <pksav/gen1/time.h>
@@ -37,33 +38,6 @@ enum pksav_gen1_save_type
  * This is used with the pksav_gen1_save_t.current_pokemon_box_num field.
  */
 #define PKSAV_GEN1_CURRENT_POKEMON_BOX_NUM_MASK ((uint8_t)0x0F)
-
-/*!
- * @brief Bitmasks for checking if the player has a specific badge.
- *
- * The pksav_gen1_save_t.badges field points to the byte in the save file
- * that stores this bitmap. Use these enum values on this byte to access and
- * set whether or not the player has this badge.
- */
-enum pksav_gen1_badge_mask
-{
-    //! Earth Badge (Giovanni, Badge #8)
-    PKSAV_GEN1_EARTH_BADGE    = 0x01,
-    //! Volcano Badge (Blaine, Badge #7)
-    PKSAV_GEN1_VOLCANO_BADGE  = 0x02,
-    //! Marsh Badge (Sabrina, Badge #6)
-    PKSAV_GEN1_MARSH_BADGE    = 0x04,
-    //! Soul Badge (Koga, Badge #5)
-    PKSAV_GEN1_SOUL_BADGE     = 0x08,
-    //! Rainbow Badge (Erika, Badge #4)
-    PKSAV_GEN1_RAINBOW_BADGE  = 0x10,
-    //! Thunder Badge (Lt. Surge, Badge #3)
-    PKSAV_GEN1_THUNDER_BADGE  = 0x20,
-    //! Cascade Badge (Misty, Badge #2)
-    PKSAV_GEN1_CASCADE_BADGE  = 0x40,
-    //! Boulder Badge (Brock, Badge #1)
-    PKSAV_GEN1_BOULDER_BADGE  = 0x80
-};
 
 struct pksav_gen1_pokedex_lists
 {
