722f91a3d0684ecb42871aa2f92ec4f540f1f8ba
Nicholas Corgan <n.corgan@gmail.com>
2018-05-02 16:23:30 -0500

    pokerus: functions no longer inline

--- include/pksav/common/pokerus.h
+++ include/pksav/common/pokerus.h
@@ -8,6 +8,7 @@
 #define PKSAV_COMMON_POKERUS_H
 
 #include <pksav/config.h>
+#include <pksav/error.h>
 
 #include <stdint.h>
 
@@ -26,85 +27,25 @@ enum pksav_pokerus_strain
 extern "C" {
 #endif
 
-static inline enum pksav_error pksav_pokerus_get_strain(
+PKSAV_API enum pksav_error pksav_pokerus_get_strain(
     const uint8_t* pokerus_ptr,
     enum pksav_pokerus_strain* strain_out
-)
-{
-    if(!pokerus_ptr)
-    {
-        return PKSAV_ERROR_NULL_POINTER;
-    }
-    if(!strain_out)
-    {
-        return PKSAV_ERROR_NULL_POINTER;
-    }
+);
 
-    *strain_out = (enum pksav_pokerus_strain)((((*pokerus_ptr) & PKSAV_POKERUS_STRAIN_MASK) >> 4) % 4);
-
-    return PKSAV_ERROR_NONE;
-}
-
-static inline enum pksav_error pksav_pokerus_set_strain(
+PKSAV_API enum pksav_error pksav_pokerus_set_strain(
     uint8_t* pokerus_ptr,
     enum pksav_pokerus_strain strain
-)
-{
-    if(!pokerus_ptr)
-    {
-        return PKSAV_ERROR_NULL_POINTER;
-    }
+);
 
-    *pokerus_ptr = (strain << 4) | ((strain % 4) + 1);
-
-    return PKSAV_ERROR_NONE;
-}
-
-static inline enum pksav_error pksav_pokerus_get_duration(
+PKSAV_API enum pksav_error pksav_pokerus_get_duration(
     const uint8_t* pokerus_ptr,
     uint8_t* duration_out
-)
-{
-    if(!pokerus_ptr)
-    {
-        return PKSAV_ERROR_NULL_POINTER;
-    }
-    if(!duration_out)
-    {
-        return PKSAV_ERROR_NULL_POINTER;
-    }
+);
 
-    *duration_out = ((*pokerus_ptr) & PKSAV_POKERUS_DURATION_MASK);
-
-    return PKSAV_ERROR_NONE;
-}
-
-static inline enum pksav_error pksav_pokerus_set_duration(
+PKSAV_API enum pksav_error pksav_pokerus_set_duration(
     uint8_t* pokerus_ptr,
     uint8_t duration
-)
-{
-    if(!pokerus_ptr)
-    {
-        return PKSAV_ERROR_NULL_POINTER;
-    }
-    if(duration > 15)
-    {
-        return PKSAV_ERROR_PARAM_OUT_OF_RANGE;
-    }
-
-    *pokerus_ptr &= ~PKSAV_POKERUS_DURATION_MASK;
-    *pokerus_ptr |= duration;
-
-    // Adjust the strain if the duration is incompatible.
-    if(duration >= 12)
-    {
-        *pokerus_ptr &= ~PKSAV_POKERUS_STRAIN_MASK;
-        *pokerus_ptr |= ((duration % 4) << 4);
-    }
-
-    return PKSAV_ERROR_NONE;
-}
+);
 
 #ifdef __cplusplus
 }
